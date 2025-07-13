bc8fbe9f4ec78f54d2cf40e624434c4f6d28e221
Jimmy abraham <abrahamjimmy@hotmail.com>
2025-07-13 13:03:38 +0200

    Deactivated hardtime

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,20 +1,20 @@
 return {
-
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    opts = {
-      notification = true,
-      custom_notify = function(msg)
-        require("notify")(msg, "warn", { title = "Hardtime" })
-      end,
-    },
-    keys = {
-      { "<leader>ht", "<cmd>Hardtime toggle<cr>",  desc = "Toggle hardtime" },
-      { "<leader>he", "<cmd>Hardtime enable<cr>",  desc = "Enable hardtime" },
-      { "<leader>hd", "<cmd>Hardtime disable<cr>", desc = "Disable hardtime" }
-
-    }
-  }
+  --
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   lazy = false,
+  --   dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+  --   opts = {
+  --     notification = true,
+  --     custom_notify = function(msg)
+  --       require("notify")(msg, "warn", { title = "Hardtime" })
+  --     end,
+  --   },
+  --   keys = {
+  --     { "<leader>ht", "<cmd>Hardtime toggle<cr>",  desc = "Toggle hardtime" },
+  --     { "<leader>he", "<cmd>Hardtime enable<cr>",  desc = "Enable hardtime" },
+  --     { "<leader>hd", "<cmd>Hardtime disable<cr>", desc = "Disable hardtime" }
+  --
+  --   }
+  -- }
 }
