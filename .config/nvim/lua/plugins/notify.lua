98e207987b290842bd7c8499e6046ae57b726fa7
Jimmy Abraham <abrahamjimmy@hotmail.com>
2026-03-02 16:48:39 +0100

    [TMUX] Updated conf [NVIM]  split statusline to its own file, updated treesitter, minor notify fix

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -1,39 +1,45 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "rcarriga/nvim-notify",
-    lazy = false, -- load only when required
-    dependencies = { "telescope.nvim" },
-    config = function()
-      require("notify").setup({
-        stages = "fade",
-        timeout = 3500,
-        background_colour = "#1e1e2e",
-      })
-      vim.notify = require("notify")
-    end,
-    keys = {
-      {
-        "<leader>nq",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss notifications"
-      },
-      {
-        "<leader>ns",
-        function()
-          require("notify")._print_history()
-        end,
-        desc = "Show notification"
-      },
-      { "<leader>nh", ":Notifications<cr>",    desc = "Show notification history" },
-      { "<leader>nt", ":Telescope notify<cr>", desc = "Show notification history, with telescope" },
-    }
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                              , branch = '0.1.x',
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- "nvim-treesitter/nvim-treesitter",
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		lazy = false, -- load only when required
+		dependencies = {
+			"telescope.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("notify").setup({
+				stages = "fade",
+				timeout = 3500,
+				background_colour = "#1e1e2e",
+			})
+			vim.notify = require("notify")
+		end,
+		keys = {
+			{
+				"<leader>nq",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss notifications",
+			},
+			{
+				"<leader>ns",
+				function()
+					require("notify")._print_history()
+				end,
+				desc = "Show notification",
+			},
+			{ "<leader>nh", ":Notifications<cr>", desc = "Show notification history" },
+			{ "<leader>nt", ":Telescope notify<cr>", desc = "Show notification history, with telescope" },
+		},
+	},
 }
